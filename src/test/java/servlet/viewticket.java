de53f29fc5c82ce6f3461af14dcd6d59a9114ce3
sTingley <tings@protonmail.com>
2021-02-17 17:03:32 -0500

    test

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -4,7 +4,7 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
-//comment
+
 public class viewticket extends HttpServlet {
 	
 	public void service(HttpServletRequest req,HttpServletResponse res)
